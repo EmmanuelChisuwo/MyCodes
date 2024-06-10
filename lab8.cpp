219550ec547c61c40813dc1fa719bf61071cb510
Emmanuel Chisuwo <Bsc-com-ne-16-21@unima.ac.mw>
2024-06-10 21:34:16 +0200

    MyCodes

---
+++ lab8.cpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int list[6] = {2,4,6,2,8,9};
+cout << list<<endl;
+
+return 0;
+}   
+ int animals() {
+
+    int mano = 23;
+    int* pPointer=nullptr;
+    pPointer= &mano;
+     pPointer = 122;
+     cout << mano<<endl;
+     
+     return 0;  
+ }
